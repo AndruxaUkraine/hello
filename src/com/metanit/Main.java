d464775b289f6cc6de14e8bb56734c0a73e8f0f1
Хмельничий Андрій Вікторович <a.hmelnychyi@AZOT.UA>
2020-09-14 13:43:55 +0300

    Initial commit

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,26 @@
+package com.metanit;
+
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        //System.out.print("два");
+        System.out.print(b);
+        System.out.print(" плюс ");
+        //System.out.print(" минус ");
+        System.out.print(a);
+        //System.out.print("три");
+        System.out.print(" равно ");
+        //System.out.print(" будет ");
+        //System.out.print("пять");
+        System.out.print(a + b);
+
+    }
+
+}
